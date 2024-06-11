d237eb4a2f37f38183e9dea8f1cf91b561dd9695
Midhun K <midhunkkpm@gmail.com>
2024-06-12 01:08:25 +0530

    added location html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",,
-  
+  content: ["./src/**/*.{html,js}",
   ]
   ,
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'primary-grey' : "#797979",
+        'primary-yellow' : "#FFD542",
+        "primary-black" : "#242424",
+        'primary-white' : "#f6f6f6",
+        'primary-blue' : "#1c4ed9"
+      }
+    },
   },
   plugins: [
-   
+
   ],
 }
 
