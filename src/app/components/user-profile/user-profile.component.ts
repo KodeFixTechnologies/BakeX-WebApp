15d4d2b27e1316df0ce1793d40d057bd4081632e
Midhun K <midhunkkpm@gmail.com>
2024-06-12 18:20:18 +0530

    FlowBite

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'user-profile',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+   constructor(
+    private dataService:DataService
+   )
+   {
+ 
+   }
+  ngOnInit(): void {
+
+    this.dataService.setData(true)
+    throw new Error('Method not implemented.');
+  }
+
+
 
 }
