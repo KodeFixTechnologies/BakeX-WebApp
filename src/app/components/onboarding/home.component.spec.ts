31d39dc1cbc2e0d68eb56be180e40b4dbf98b231
Midhun K <midhunkkpm@gmail.com>
2024-07-20 18:39:13 +0530

    fix

--- src/app/components/onboarding/home.component.spec.ts
+++ src/app/components/onboarding/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { OnboardingComponent } from './home.component';
 
 describe('OnboardingComponent', () => {
   let component: OnboardingComponent;
