a4580c18550cc77e67f369b666d75224ddc90c0b
Midhun K <midhunkkpm@gmail.com>
2024-08-05 18:32:25 +0530

    seo

--- src/app/components/onboarding/home.component.ts
+++ src/app/components/onboarding/home.component.ts
@@ -7,7 +7,7 @@ import { Users } from '../../models/user';
 import { AuthService } from '../../services/auth.service';
 import { QueryService } from '../../services/query.service';
 import { FormsModule } from '@angular/forms';
-
+import { Meta, Title } from '@angular/platform-browser';
 @Component({
     selector: 'home',
     standalone: true,
@@ -27,7 +27,8 @@ export class OnboardingComponent implements OnInit {
         private dataService:DataService,
         private authService:AuthService,
         private queryService:QueryService,
-        private platFormLocation:PlatformLocation
+        private platFormLocation:PlatformLocation,
+        private meta: Meta, private title: Title
     ) { 
 
             //prevent backward navigationcode
@@ -38,7 +39,19 @@ export class OnboardingComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      
+
+        this.title.setTitle('Bakejoli - Connecting jobseekers with Job Opportunities in the Food Industry');
+        this.meta.addTags([
+          { name: 'description', content: 'Bakejoli connects bakers with employment opportunities in the food industry. Find jobs, hire talent, and streamline your bakery business. Join over 5000 bakers today.' },
+          { name: 'keywords', content: 'hotel jobs kerala, bakery employment, kerala jobs, bakejoli, food industry jobs,kerala food jobs,' },
+          { name: 'author', content: 'Bakejoli' },
+          { name: 'robots', content: 'index, follow' },
+          { property: 'og:title', content: 'BakeJoli - Connecting job seekerks with Job Opportunities in the Food Industry' },
+          { property: 'og:description', content: 'Bakejoli connects job seekerks with employment opportunities in the food industry. Find jobs, hire talent, and streamline your bakery business. Join over 5000 bakers today.' },
+          { property: 'og:url', content: 'https://bakejoli.com' },
+          { property: 'og:type', content: 'website' },
+
+        ]);
         this.queryService.getPricingData().subscribe(data => {
             this.pricingData = data;
             this.isLoading = false; 
